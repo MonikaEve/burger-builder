c819350e6e246aa256a56ec78c888bcc587d2be7
Monika Eversmann <moni.eversmann@gmail.com>
2019-03-08 16:54:56 +0100

    api

--- axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
- // this url shouldn't be global because authentication will use a different url
-const instance = axios.create({
-    baseURL:'https://react-my-burger-d157c.firebaseio.com'
-});
-
-export default instance;
